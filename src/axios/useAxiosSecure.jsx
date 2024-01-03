5a0c007b46129eae8196b7fdc498924ac11d7b2b
sharifrayhan <sharifrayhan.official@gmail.com>
2024-01-03 21:14:23 +0600

    replace server url

--- src/axios/useAxiosSecure.jsx
+++ src/axios/useAxiosSecure.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://contact-management-server-beta.vercel.app/",
 });
 
 const useAxiosSecure = () => {
